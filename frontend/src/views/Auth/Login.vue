2247fa42542ce9681f187c04a64d789154e0f623
Monarch Wadia <monarch@zeroprojects.ca>
2020-06-06 00:44:49 -0400

    Login: Email is now focused on mount

--- frontend/src/views/Auth/Login.vue
+++ frontend/src/views/Auth/Login.vue
@@ -3,7 +3,7 @@
     form.login-form(v-on:submit.prevent="onSubmit")
       h1 Login
       label Email
-        input(name="email", v-model="email")
+        input(name="email", v-model="email" ref="emailInput")
       label Password
         input(name="password", type="password", v-model="password")
       button(type="submit") Submit
@@ -27,6 +27,9 @@ export default {
       const { email, password, $router } = this;
       this.$store.dispatch('login', { email, password, $router });
     }
+  },
+  mounted() {
+    this.$refs.emailInput && this.$refs.emailInput.focus();
   }
 };
 </script>
