cdef2d98bdaeeb80e421f84a5096adf0d3d26963
Monarch Wadia <monarch@zeroprojects.ca>
2020-06-06 02:21:18 -0400

    Created fresh user seed

--- backend/src/db/seeders/99-Fresh User.js
+++ backend/src/db/seeders/99-Fresh User.js
@@ -1,22 +1,16 @@
 'use strict';
 
 const { User } = require('../models');
-
 const userFactory = require('../factories/user.factory');
-
-const TEST_EMAIL = "user$@mintbean.io";
 const TEST_PASSWORD = "password";
 
 module.exports = {
   up:  async (queryInterface, Sequelize) => {
-    // create user1, user2, user3...@mintbean.io
-    const users = userFactory.bulk(10);
-    for (var i = 1; i <= 9; i++) {
-      const u = users[i];
-      u.email = TEST_EMAIL.replace('$', i);
-      u.password_hash = TEST_PASSWORD; // TODO: change
-      await (User.create(u));
-    }
+    // create user@mintbean.io
+    await User.create(userFactory.one({
+      email: 'user@mintbean.io',
+      password_hash: TEST_PASSWORD
+    }));
 
     /*
       Add altering commands here.
