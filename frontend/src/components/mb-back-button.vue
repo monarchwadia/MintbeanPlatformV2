b702b78750fe04292eba6f74ebd866aa9fe66cdc
Monarch Wadia <monarch@zeroprojects.ca>
2020-07-02 09:53:25 -0400

    Added back button component

---
+++ frontend/src/components/mb-back-button.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div.text-2xl.cursor-pointer.inline-block.hover_text-grey-100(v-on:click.prevent="goBack" v-bind="{...$props, ...$attrs}")
+    fa.icon.mr-2(icon="arrow-left")
+    | Back
+</template>
+
+<script>
+export default {
+  name: "mb-back-button",
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  }
+}
+</script>
--- frontend/src/views/Project.vue
+++ frontend/src/views/Project.vue
@@ -1,68 +1,66 @@
 <template lang="pug">
-div.project-wrapper
-  div.project-wrapper-inner
-    div.background-banner
-    main.project
-      div(v-if="!!project")
-        h1 {{ project.title }}
-        h3 Author
-        p {{ project.User.firstname }} {{ project.User.lastname }}
-        h3(v-if="project.source_code_url") Source Code
-        mb-external-link(:href="project.source_code_url") Click here to see source code
-        h3(v-if="project.live_url") View Project
-        mb-external-link(:href="project.live_url") Click here to view the project
-        div.screenshots
-          h2 Media
-          section
-            aside(v-for="mediaAsset in project.MediaAssets")
-              mb-image-display(v-if="mediaAsset.cloudinaryPublicId" :publicId="mediaAsset.cloudinaryPublicId", width="1000" height="1000")
-              div(v-if="isAdmin")
-                button(v-if="mediaAsset.cloudinaryPublicId" v-on:click.prevent="handleDeleteMediaAsset(mediaAsset)") Delete
-          section(v-if="!project.MediaAssets.length")
-            p No media uploaded.
-            section(v-if="isAdmin")
-              aside
-                div.flex
-                  label Upload File
-                    mb-file-upload(:files="[]" name="files" ref="mbFileUpload")
-                    button(v-on:click.prevent="handleUploadMediaAssets()") Submit
+div.container.m-auto
+  mb-back-button
 
-        h1 Score: {{averageScore}} / 10
-        h1 Votes
-        section(v-if="!project.Votes || project.Votes.length === 0")
-          p No comments yet
-        section(v-else)
-          div.vote-comment(v-for="vote in project.Votes" style="width: 100%;")
-            b {{vote.User.firstname + ' ' + vote.User.lastname}}
-            p {{ vote.comment }}
-        section(v-if="$store.state.user")
-          form.vote-project(v-on:submit.prevent="function(evt){ handleVoteProject(project.id) }")
-            h2 {{ userVote ? 'Edit' : 'Submit' }} a vote
-            p {{ userVote ? 'Editing' : 'Editing' }} as {{ $store.state.user.firstname }} {{ $store.state.user.lastname }}
-            label Rating (1 - 10)
-              select(v-model="rating")
-                option 10
-                option 9
-                option 8
-                option 7
-                option 6
-                option 5
-                option 4
-                option 3
-                option 2
-                option 1
-            label Comment
-              textarea(name="comment", v-model="comment")
-            input(type="submit" :value="userVote ? 'Edit' : 'Submit'")
-        section(v-else)
-          h2 You must be logged in to vote
-        section.vote-submit-section(v-if="userVote")
-          aside(style="width: 100%;")
-            h3 (Your previous vote:)
-            h2 {{userVote.User.firstname + ' ' + userVote.User.lastname}}
-            h3 Rating: {{ userVote.rating }} / 10
-            h3 Comments:
-            p {{ userVote.comment }}
+  div(v-if="!!project")
+    h1.text-5xl {{ project.title }}
+    p.text-xl by {{ project.User.firstname }} {{ project.User.lastname }}
+    h3(v-if="project.source_code_url") Source Code
+    mb-external-link(:href="project.source_code_url") Click here to see source code
+    h3(v-if="project.live_url") View Project
+    mb-external-link(:href="project.live_url") Click here to view the project
+    div
+      h2 Media
+      section
+        aside(v-for="mediaAsset in project.MediaAssets")
+          mb-image-display(v-if="mediaAsset.cloudinaryPublicId" :publicId="mediaAsset.cloudinaryPublicId", width="1000" height="1000")
+          div(v-if="isAdmin")
+            button(v-if="mediaAsset.cloudinaryPublicId" v-on:click.prevent="handleDeleteMediaAsset(mediaAsset)") Delete
+      section(v-if="!project.MediaAssets.length")
+        p No media uploaded.
+        section(v-if="isAdmin")
+          aside
+            div.flex
+              label Upload File
+                mb-file-upload(:files="[]" name="files" ref="mbFileUpload")
+                button(v-on:click.prevent="handleUploadMediaAssets()") Submit
+
+    h1 Score: {{averageScore}} / 10
+    h1 Votes
+    section(v-if="!project.Votes || project.Votes.length === 0")
+      p No comments yet
+    section(v-else)
+      div(v-for="vote in project.Votes" style="width: 100%;")
+        b {{vote.User.firstname + ' ' + vote.User.lastname}}
+        p {{ vote.comment }}
+    section(v-if="$store.state.user")
+      form(v-on:submit.prevent="function(evt){ handleVoteProject(project.id) }")
+        h2 {{ userVote ? 'Edit' : 'Submit' }} a vote
+        p {{ userVote ? 'Editing' : 'Editing' }} as {{ $store.state.user.firstname }} {{ $store.state.user.lastname }}
+        label Rating (1 - 10)
+          select(v-model="rating")
+            option 10
+            option 9
+            option 8
+            option 7
+            option 6
+            option 5
+            option 4
+            option 3
+            option 2
+            option 1
+        label Comment
+          textarea(name="comment", v-model="comment")
+        input(type="submit" :value="userVote ? 'Edit' : 'Submit'")
+    section(v-else)
+      h2 You must be logged in to vote
+    section(v-if="userVote")
+      aside(style="width: 100%;")
+        h3 (Your previous vote:)
+        h2 {{userVote.User.firstname + ' ' + userVote.User.lastname}}
+        h3 Rating: {{ userVote.rating }} / 10
+        h3 Comments:
+        p {{ userVote.comment }}
 
 </template>
 
@@ -110,6 +108,9 @@ export default {
     }
   },
   methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
     handleVoteProject(ProjectId) {
       const { id } = this.$store.state.project;
       const { comment, rating } = this;
