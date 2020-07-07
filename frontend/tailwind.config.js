36971d0cf6d9be15f5bf9577b92553b604577aa6
claire froelich <claire.froelich@gmail.com>
2020-07-07 17:09:08 -0400

    add smoke color set to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     fontFamily: {
       main: ["Montserrat"]
     },
-    extend: {}
+    extend: {},
+    colors: {
+      'smoke-darkest': 'rgba(0, 0, 0, 0.9)',
+      'smoke-darker': 'rgba(0, 0, 0, 0.75)',
+      'smoke-dark': 'rgba(0, 0, 0, 0.6)',
+      'smoke': 'rgba(0, 0, 0, 0.5)',
+      'smoke-light': 'rgba(0, 0, 0, 0.4)',
+      'smoke-lighter': 'rgba(0, 0, 0, 0.25)',
+      'smoke-lightest': 'rgba(0, 0, 0, 0.1)',
+    }
   },
   variants: {
     boxShadow: ['hover']
