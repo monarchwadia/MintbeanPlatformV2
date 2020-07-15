29be77d273dd83ee1981fc1f020c0e7e8e43f47f
claire froelich <claire.froelich@gmail.com>
2020-07-15 14:40:29 -0400

    clean more

--- frontend/src/components/mb-event-card.vue
+++ frontend/src/components/mb-event-card.vue
@@ -2,7 +2,7 @@
   mb-internal-link(:to="'/mb-event/' + id")
     div.rounded.overflow-hidden.shadow-xl.hover_shadow-2xl(style="width: 500px;")
       div.w-full.relative(style="background-size: cover; height: 225px;" :style="{ backgroundImage: urlFor(cloudinaryPublicId) }")
-        mb-a-button.absolute.z-99.right-0.gradient-blue-mint.text-white.p-2.hover_shadow-md(isExternal :href="registerLink" @click.stop) Register
+        mb-a-button.absolute.z-99.right-0(isExternal :href="registerLink" @click.stop) Register
       div.px-6.py-4.text-gray-700.flex.justify-between
         div.text-xl {{ eventTitle }}
         div {{prettyDate(startTime) + ' (EST)'}}
