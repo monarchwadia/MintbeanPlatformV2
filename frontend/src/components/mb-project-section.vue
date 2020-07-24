fbf03cd4db6f881305c874bf40674b7bb48335a9
claire froelich <claire.froelich@gmail.com>
2020-07-24 09:09:12 -0700

    fix homepage errors

--- frontend/src/components/mb-project-section.vue
+++ frontend/src/components/mb-project-section.vue
@@ -17,6 +17,7 @@
       //- )
     div.sliding-section.flex.justify-between.overflow-x-auto(:class="sliderIsDown ? 'active' : null" ref="projectSectionContainer")
       mb-project-card.mr-4(
+        v-if="project"
         v-for="project in projects"
         :key="project.id"
         :id="project.id"
@@ -35,7 +36,7 @@ export default {
     title: String,
     projects: {
       type: Array,
-      default: () => [],
+      default: () => []
     }
   },
   data() {
@@ -43,12 +44,12 @@ export default {
       editTitle: this.title,
       sliderIsDown: false,
       startX: null,
-      scrollLeft: null,
-    }
+      scrollLeft: null
+    };
   },
   methods: {
     deleteSection() {
-      alert('faking deletion of this section');
+      alert("faking deletion of this section");
     },
     openModal() {
       this.$refs.modalEdit.open();
@@ -58,77 +59,76 @@ export default {
       this.$refs.modalEdit.$refs.modal.close();
     },
     getFullname(user) {
-      return `${user.firstname} ${user.lastname}`
-    },
+      return `${user.firstname} ${user.lastname}`;
+    }
   },
   computed: {
     isAdmin: function() {
       return this.$store.state.user && this.$store.state.user.isAdmin;
-    },
+    }
   },
   mounted() {
     // listen for horizontal scroll drag (https://codepen.io/thenutz/pen/VwYeYEE)
-    this.$refs.projectSectionContainer.addEventListener('mousedown', (e) => {
+    this.$refs.projectSectionContainer.addEventListener("mousedown", e => {
       this.sliderIsDown = true;
       this.startX = e.pageX - this.$refs.projectSectionContainer.offsetLeft;
       this.scrollLeft = this.$refs.projectSectionContainer.scrollLeft;
     });
-    this.$refs.projectSectionContainer.addEventListener('mouseleave', () => {
+    this.$refs.projectSectionContainer.addEventListener("mouseleave", () => {
       this.sliderIsDown = false;
     });
-    this.$refs.projectSectionContainer.addEventListener('mouseup', () => {
+    this.$refs.projectSectionContainer.addEventListener("mouseup", () => {
       this.sliderIsDown = false;
     });
-    this.$refs.projectSectionContainer.addEventListener('mousemove', (e) => {
-      if(!this.sliderIsDown) return;
+    this.$refs.projectSectionContainer.addEventListener("mousemove", e => {
+      if (!this.sliderIsDown) return;
       e.preventDefault();
       const x = e.pageX - this.$refs.projectSectionContainer.offsetLeft;
       const walk = (x - this.startX) * 3; //scroll-fast
       this.$refs.projectSectionContainer.scrollLeft = this.scrollLeft - walk;
     });
   }
-}
+};
 </script>
 
-<style lang="css" scoped >
-  .sliding-section.active {
-    cursor: ew-resize;
-    transform: scale(1.01);
-    transition: ease 0.2s;
-  }
-  .sliding-section {
-    transition: ease 0.2s;
-    cursor: ew-resize;
-  }
+<style lang="css" scoped>
+.sliding-section.active {
+  cursor: ew-resize;
+  transform: scale(1.01);
+  transition: ease 0.2s;
+}
+.sliding-section {
+  transition: ease 0.2s;
+  cursor: ew-resize;
+}
 
-  /* width */
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
 
-  /* Track */
-  ::-webkit-scrollbar-track {
-    background: rgba(2, 237, 157, 0.6);
-    background: -moz-linear-gradient(
-      90deg,
-      rgba(2, 237, 157, 0.6) 0%,
-      rgba(0, 155, 226, 0.6) 100%
-    );
-    background: linear-gradient(
-      90deg,
-      rgba(2, 237, 157, 0.6) 0%,
-      rgba(0, 155, 226, 0.6) 100%
-    );
-  }
+/* Track */
+::-webkit-scrollbar-track {
+  background: rgba(2, 237, 157, 0.6);
+  background: -moz-linear-gradient(
+    90deg,
+    rgba(2, 237, 157, 0.6) 0%,
+    rgba(0, 155, 226, 0.6) 100%
+  );
+  background: linear-gradient(
+    90deg,
+    rgba(2, 237, 157, 0.6) 0%,
+    rgba(0, 155, 226, 0.6) 100%
+  );
+}
 
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    background: #888;
-  }
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
 
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: #555;
-  }
-
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
