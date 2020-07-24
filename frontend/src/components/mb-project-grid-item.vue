aa045349b6287f5fa6978b894fe2c791ef59dd51
claire froelich <claire.froelich@gmail.com>
2020-07-24 11:45:05 -0700

    add fix for project cards with no cloudinary iamge

--- frontend/src/components/mb-project-grid-item.vue
+++ frontend/src/components/mb-project-grid-item.vue
@@ -2,12 +2,16 @@
 mb-external-link(:href="projectDeployedUrl")
   div.shadow-mb.m-2.mb-10.hover_shadow-2xl.cursor-pointer(style="max-width: 400px; background: #3d3d3d;")
     div.inner
-      div.screenshot.relative(style="min-height: 300px;" :style="{background: urlFor(project.mbevent_cover_image_url), backgroundPosition: 'center', backgroundSize: 'cover'}")
+      div.screenshot.relative(:style="{background: urlFor(project.mbevent_cover_image_url), backgroundPosition: 'center', backgroundSize: 'cover', minHeight: !cloudinaryPublicId ? '300px' : 0}")
         div.overlay-top.absolute.text-white.py-3.top-0.z-50.w-full(style="background: linear-gradient(180deg, #3d3d3d, transparent);")
 
         div.overlay-bottom.absolute.flex.items-center.justify-between.text-white.py-2.bottom-0.text-right.z-50.w-full(style="background: linear-gradient(0deg, #3d3d3d,transparent);")
 
-        mb-image-display.relative(:publicId="cloudinaryPublicId" height="300" width="400")
+        mb-image-display.relative(v-if="!!cloudinaryPublicId" :publicId="cloudinaryPublicId" height="300" width="400")
+        div(v-else)
+          div._hide-on-sm(style="width: 400px")
+          div._show-on-sm.hidden
+
       div.p-4.text-white.flex.flex-col.justify-between(style="min-height:166px")
         div
           div.flex.justify-between.items-start.mb-2
@@ -26,6 +30,19 @@ mb-external-link(:href="projectDeployedUrl")
           mb-a-button.w-full.text-center(isExternal :href="projectPlatformUrl" ) Comments
           //- mb-a-button(isExternal :href="projectDeployedUrl") Demo
 </template>
+
+<style lang="scss" scoped>
+// spacers for cards with no cloudinary image
+@media only screen and (max-width: 457px) {
+  ._hide-on-sm {
+    display: none;
+  }
+  ._show-on-sm {
+    display: block;
+    width: calc(100vw - 4rem);
+  }
+}
+</style>
 
 <script>
 export default {
@@ -51,7 +68,7 @@ export default {
     },
     cloudinaryPublicId() {
       const { cloudinaryPublicId, mbevent_cover_image_url } = this.project;
-      return cloudinaryPublicId || mbevent_cover_image_url;
+      return cloudinaryPublicId || null;
     }
   },
   methods: {
