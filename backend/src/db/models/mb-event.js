331a8044dd6ae63381bcd98cd4b2deafe1bdde91
claire froelich <claire.froelich@gmail.com>
2020-08-04 14:14:41 -0700

    add register link to events - show register button for upcoming events

--- backend/src/db/models/mb-event.js
+++ backend/src/db/models/mb-event.js
@@ -1,13 +1,18 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const MbEvent = sequelize.define('MbEvent', {
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    cover_image_url: DataTypes.TEXT,
-    instructions: DataTypes.TEXT,
-    start_time: DataTypes.DATE,
-    end_time: DataTypes.DATE
-  }, {});
+  const MbEvent = sequelize.define(
+    "MbEvent",
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      cover_image_url: DataTypes.TEXT,
+      instructions: DataTypes.TEXT,
+      start_time: DataTypes.DATE,
+      end_time: DataTypes.DATE,
+      register_link: DataTypes.STRING
+    },
+    {}
+  );
   MbEvent.associate = function(models) {
     MbEvent.hasMany(models.Project);
     // associations can be defined here
